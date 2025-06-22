ed6fa944d14987587262509d7902a2937dfaa962
AndyIsHigh <126885515+AndyIsHigh@users.noreply.github.com>
2025-06-23 00:45:50 +0800

    Api fetch update
    - Added logging configuration
    - Replaced print statement with logging statement

--- datagov_api.py
+++ datagov_api.py
@@ -1,11 +1,20 @@
+import logging.config
 import requests
 import json
 import time
+import logging
 from typing import Optional, Literal, Dict, Any, List
+
+logging.basicConfig(
+    filename='api_call.log',
+    filemode='a',
+    level=logging.DEBUG,
+    format='%(asctime)s | %(levelname)s | %(message)s'
+)
 
 CatalogueItem = Literal["fuelprice"]
 
-#TODO: implement retry mechanism and replace print statement with logger
+#TODO: add start and end date optional parameter
 def _fetch_data(item_id: CatalogueItem, limit: Optional[int] = None) -> Optional[List[Dict[str, Any]]]:
     base_url = 'http://api.data.gov.my/data-catalogue'
     request_params = {
@@ -17,19 +26,20 @@ def _fetch_data(item_id: CatalogueItem, limit: Optional[int] = None) -> Optional
         response = requests.get(base_url, params=request_params, timeout=10)
         response.raise_for_status()  # Raises an HTTPError for bad responses
         data = response.json()
+        logging.info("Data fetch successfully, item id: %s", item_id)
         return data
         
     except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
+        logging.error("HTTP error occurred: %s", err)
 
     except requests.exceptions.ConnectionError as err:
-        print(f"Connection error occurred: {err}")
+        logging.error("Connection error occurred: %s", err)
 
     except requests.exceptions.Timeout as err:
-        print(f"Timeout error occurred: {err}")
+        logging.error("Timeout error occurred: %s", err)
 
     except requests.exceptions.RequestException as err:
-        print(f"An error occurred: {err}")
+        logging.error("An error occurred: %s", err)
 
 def fetch_fuel_price(limit: Optional[int] = None, retry: int = 3) -> Optional[List[Dict[str, Any]]]:
     for _ in range(retry):
